ab8313042dbdc47deb11d5f371e29cdd8219e87c
jasonmpe <wanght1991@gmail.com>
2015-10-24 15:52:19 -0800

    js update

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+	$('.link .button').hover(function(){
+		var title = $(this).attr('data');
+		$('.tip em').text(title);
+		var pos=$(this).position().left+10;
+		var dis = ($('.tip').outerWidth()-$(this).outerWidth())/2;
+		var l = pos- dis;
+		$('.tip').css({'left':l+'px'}).animate({'top':145,'opacity':1},300);
+	},function(){
+		if(!$('tip').is(':animated')){
+			$('.tip').stop(true).animate({'top':100,'opacity':0}, 300);
+		}
+	});
+});
